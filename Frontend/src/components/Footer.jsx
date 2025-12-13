6296d5dbc0b163e35d2c8182c152fd979d9b868c
chinky23-tech <chinky0330@gmail.com>
2025-12-13 23:27:40 +0530

    update

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -6,7 +6,7 @@
                 <li><a href="#">Home</a></li>
                 <li><a href="#">Services</a></li>
                 <li><a href="#">About</a></li>
-                 <li><a href="#">About</a></li>
+               
             </ul>
         </footer>
     );
