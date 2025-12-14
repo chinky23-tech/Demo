5c8c1ca7dd74642d56b47046a82f54aa01a00d55
chinky23-tech <chinky0330@gmail.com>
2025-12-14 23:01:48 +0530

    update

--- Frontend/src/components/Profile.jsx
+++ Frontend/src/components/Profile.jsx
@@ -1,3 +1,10 @@
- function Profile({props}){
-    console.log(props.name);
- }
+function Profile({name, role, description}){
+   return(
+      <div>
+     <h2>{name}</h2>
+     <h4>{role}</h4>
+     <p>{description}</p>
+     </div>
+   );
+}
+export default Profile;
